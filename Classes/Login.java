afa519f36332c6636ea5166e84ac4d1c2d1c7ffa
amado-developer <amado_developer@hotmail.com>
2020-03-14 13:13:16 -0600

    Login logic is done, gotta wait for queries and work on Main page

--- Classes/Login.java
+++ Classes/Login.java
@@ -31,7 +31,7 @@ public class Login{
       * @param id
       * @return True or false wheter a user is admin or not
       */
-     private bool isAdministrator(String userName, int id){
+     private boolean isAdministrator(String userName, int id){
         String administrator = "QUERY"
          //TODO: userName split into id and name
          //Query
@@ -43,7 +43,7 @@ public class Login{
       * @param id
       * @return True of False wheter a user is registered or not
       */
-     private bool isUser(String userName, int id){
+     private boolean isUser(String userName, int id){
          String user = "Query";
 
          return true;
@@ -54,18 +54,19 @@ public class Login{
       * Is in charge of Loging in the user
       */
      public void doLogIn(){
-        String id_UserName = this.userName.split("_");
-        String userName = id_UserName[0];
-        int id = id_UserName[1];
+        String userName_ID= this.userName.split("_");
+        String userName = userName_ID[0];
+        int id = userName_ID[1];
 
          String isAdministrator = isAdministrator(userName, id);
-         String isUser = isUser(userName, id);
-
          if(isAdministrator){
-             startSession(isAdministrator);
-             //TODO: ENTER WITH ADMINISTRATOR PRIVILEGES
-         }else if(isUser){
-             getUserRoles(id, userName)
+            startSession();
+            //TODO: ENTER WITH ADMINISTRATOR PRIVILEGES
+         }
+
+         String isUser = isUser(userName, id);
+         if(isUser){
+             getUserPermissions(id, userName)
          }else{
              //TODO: Return bad credentials message 
          }
@@ -75,18 +76,17 @@ public class Login{
      /**
       * Gets a list of permissions associated with a specific user
       */
-     private void getUserPermissions(){
+     private void getUserPermissions(int id, String userName){
          String query = "Devuelve un listado de permisos";
-         startSession(f);
+         startSession();
      }
 
      /**
       * Starts up a new session as Admin
       * @param isAdministrator
       */
-     private void startSession(boolean isAdministrator){
+     private void startSession(){
          //TODO START SESSION IF IS A ADMIN
-
      }
 
 
@@ -94,11 +94,12 @@ public class Login{
       * Starts up a new session as a common user or a user with defined permitions
       * @param permissions
       */
-     private void startSession(String [] permissions){
+     private void startSession(int [] permissions){
          if(permissions.length == 0){
              //TODO: Common user log in
          }else{
              //TODO: User with permitions log in
+             
          }
      }
 
--- Classes/Main_Page.java
+++ Classes/Main_Page.java
@@ -1,8 +1,17 @@
 public class Main_Page(){
 
-    String consult;
+    private String consult;
+    private boolean [] privileges;
     
-    public Main_Page(String consult){
+    public Main_Page(String consult, int[] permissions){
         this.consult = consult;
+        privileges = new boolean[]{false, false, false, false, false, false};
+        stablishPermissions(permission);
+    }
+
+    private void stablishPermissions(int[] permission){
+        for(int permission : permission){
+            this.privileges[permission - 1] = true;
+        }
     }
 }
